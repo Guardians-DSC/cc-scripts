f6409d526262876066a5e1c14a39046b93f4f211
Thayanne <thayannevls@gmail.com>
2018-05-11 23:35:03 -0300

    adicionando script da disciplina prog1

---
+++ prog1/prog1_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Ambiente de desenvolvimento para disciplina de Programacao 1 do primeiro periodo de computacao da UFCG
+# Autor: @thayannevls
+
+sudo apt-get update
+
+#Tst
+bash -c "$(curl -q -sSL http://bit.ly/tst-install)"
+tst update
+
+#Geany
+sudo add-apt-repository ppa:geany-dev/ppa
+sudo apt-get update
+sudo apt-get install geany
